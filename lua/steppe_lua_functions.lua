3459e91928f4a45c3f1719457d86c65c725d94eb
ForestDragon <34136200+ForestDragon-wesnoth@users.noreply.github.com>
2022-01-03 17:08:49 +0300

    added terracotta warrior

--- lua/steppe_lua_functions.lua
+++ lua/steppe_lua_functions.lua
@@ -307,8 +307,8 @@ end
 function steppe_longrange_filter(unit)
    local x,y = wesnoth.get_variable("x1"),wesnoth.get_variable("y1")
 --coordinates work flawlessly
-   wesnoth.message(unit.x)
-   wesnoth.message(unit.y)
+--   wesnoth.message(unit.x)
+--   wesnoth.message(unit.y)
 --   wesnoth.message(x)
 --   wesnoth.message(y)
 
@@ -324,40 +324,43 @@ end
 
 --music code I originally wrote for TLU
     function GSE_smooth_replace_music(new_track,fade_out_time,fade_in_time)
+        local delay_time
     
-        wesnoth.music_list.volume = 100 --percentage of the volume in settings
+        wesnoth.audio.music_list.volume = 100 --percentage of the volume in settings
     
         if fade_out_time then else
             fade_out_time = 2000-- if no delay is provided in the function, use a default value
         end
     
         delay_time = fade_out_time / 10 -- since the fade out takes roughly 10 steps, divide the time by 10
+--        wesnoth.message(fade_out_time)
     
-        while wesnoth.music_list.volume >= 10 do
-            wesnoth.music_list.volume = wesnoth.music_list.volume - 10
+        while wesnoth.audio.music_list.volume >= 10 do
+            wesnoth.audio.music_list.volume = wesnoth.audio.music_list.volume - 10
 --            repeats = repeats - 1
             wesnoth.delay(delay_time)
---            wesnoth.message(wesnoth.music_list.volume)
+--            wesnoth.message(wesnoth.audio.music_list.volume)
         end
 
-        wesnoth.music_list.volume = 0
+        wesnoth.audio.music_list.volume = 0
     
-        wesnoth.music_list.clear()
-        wesnoth.music_list.add(new_track, true)
+        wesnoth.audio.music_list.clear()
+        wesnoth.audio.music_list.add(new_track, true)
 
         if fade_in_time then else
             fade_in_time = 0-- if no delay is provided in the function, fade in is instant
         end
     
         delay_time = fade_in_time / 10 -- since the fade out takes roughly 10 steps, divide the time by 10
+--        wesnoth.message(fade_in_time)
 
-        while wesnoth.music_list.volume <= 90 do
-            wesnoth.music_list.volume = wesnoth.music_list.volume + 10
+        while wesnoth.audio.music_list.volume <= 90 do
+            wesnoth.audio.music_list.volume = wesnoth.audio.music_list.volume + 10
 --            repeats = repeats - 1
-            wesnoth.delay(delay_time)
---            wesnoth.message(wesnoth.music_list.volume)
+            wesnoth.interface.delay(delay_time)
+--            wesnoth.message(wesnoth.audio.music_list.volume)
         end
 
-        wesnoth.music_list.volume = 100
+        wesnoth.audio.music_list.volume = 100
     
     end
